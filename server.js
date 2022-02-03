4113e232340e58e72ace1ca23be95dd496335769
taylor.holmes <taylorh2343@gmail.com>
2022-02-03 17:34:52 -0600

    added candidates database to server

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// EXPRESS middleware 
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//default response for any other request (not found)
-app.use((req, res) => {
-    res.status(404).end();
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Tr@yse2343',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
-// keep at bottom
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
