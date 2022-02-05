141a7a418707b500f3731d5019fa61d271fe4367
taylor.holmes <taylorh2343@gmail.com>
2022-02-04 19:10:19 -0600

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 //connect to routes in route folder
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
